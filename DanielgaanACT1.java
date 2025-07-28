b22bcf69240af7c577ee7bea8ef0228a28440d48
danieljohn-xd <johntzylacano@gmail.com>
2025-07-28 00:13:55 -0700

    ACT1

---
+++ DanielgaanACT1.java
@@ -0,0 +1,44 @@
+package danielgaanact1;
+
+import java.util.Scanner;
+
+public class DanielgaanACT1 {
+
+   
+    public static void main(String[] args) {
+    
+            Scanner sc = new Scanner(System.in);
+            String name;
+            int subjects;
+            float gsub, total = 1;
+            System.out.print("Enter student name:");
+            name = sc.nextLine();
+            
+            System.out.print("Enter number of students:");
+            subjects = sc.nextInt();
+            
+            System.out.print("Enter grade for subject 1:");
+            gsub = sc.nextInt();
+            
+            
+            
+            for(int i = 1; i <= subjects; i++){
+                System.out.print("Enter grade for subject"+ i +":");
+                gsub = sc.nextFloat();
+                total += gsub;
+            }     
+   
+            double average = total / subjects;
+            
+            System.out.printf("\nAverage: %.2f\n", average);
+            
+            if (average >= 75) {
+                System.out.print("Remarks: PASSED");
+            } else {
+                System.out.print("Remarks: FAILED");
+            }
+            
+            sc.close();
+            
+    }
+}
