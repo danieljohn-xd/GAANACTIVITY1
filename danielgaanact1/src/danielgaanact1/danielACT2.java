61a93bf0eee913a27ffcfe5f235432cb8d2ae4db
Kingolinar7 <monedakingzy@gmail.com>
2025-07-29 23:23:34 -0700

    added act 2 file

---
+++ danielgaanact1/src/danielgaanact1/danielACT2.java
@@ -0,0 +1,20 @@
+package danielgaanact1;
+
+import java.util.Scanner;
+
+public class danielACT2 (
+
+public static void main(String[] args) { Scanner sc = new Scanner(System.in);
+System.out.print ("Enter employee name: "); string ename sc.nextLine();
+System.out.print ("Enter hourly rate: ");
+float hratesc.nextFloat();
+System.out.print ("Enter total hours worked: ");
+float hours sc.nextFloat();
+System.out.println("- ------Wage Summary--
+System.out.println("Employee: "+ename);
+System.out.println("Hourly Rate: P"+hrate);
+System.out.println("Hours Worked: "+hours);
+System.out.println("Gross Weekly Wage: "+(hours *hrate));
+System.out.printf("sss Contributions (10%): P.2f) \n", ((hours *hrate) *0.1)); System.out.println("-➖➖➖➖
+----");
+System.out.printf("Net Weekly Wage: P%.2f\n\n", ((hours*hrate) ( (hours *hrate) *0.1)));
